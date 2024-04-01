268e49c199069b50af0facb87681eb100e25dccb
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2024-04-01 21:18:40 +0200

    I added new functions

--- script.js
+++ script.js
@@ -1,48 +1,115 @@
 // TASK MANAGER
 
 // taskManager object
-let task ={
-  description: "",
-  completed: false
-}
+let description ="";
 const taskManager = {
   // fill up with properties, see instructions
   tasks: [ ],
-  addTask: function (description) {
-    const taskId = this.generateUniqueId();
+  addTask: function(description){
     const newTask = {
-      id: taskId,
+      id: this.generateUniqueId(),
       description: description,
       completed:false
     }
     this.tasks.push(newTask)
-  }
+  },
+  generateUniqueId: function() {
+    return this.tasks.length + 1;
+  }, 
+ 
+ completeTask: function(taskId) {
   
+    this.tasks.map(function(task){
+    if(task.id == taskId ){
+      task.complete = precioDescuento;
+    }
+}
+ )},
+listAllTasks: function(){
+  let taskList=[];
+  this.tasks.forEach(function(task) { 
+  let taskItem = "ID: " + task.id + " - Description: " + task.description + " - Completes: " + task.completed;
+  taskList.push(taskItem);
+  });
+  return taskList;
+},
+
+listCompletedTasks: function(){
+completedTaskList = this.tasks.filter(task => task.completed == true)
+this.completedTaskList.forEach(function(task) {
+  var taskItem = document.createElement("li");
+  taskItem.textContent = "ID: " + task.id + " - Descripction: " + task.description + " - Completes: " + task.completed;
+  taskList.appendChild(taskItem);
+  });
+  return taskList;
+},
 };
 
 
 // function for asking the user to fill in their name
-function askUserName() {
-  // add logic here to as the user to enter their name
-  alert(
-    "This is the alert() function which dispays some text for the user.\n" +
-      "Unlike the prompt(), the alert() does not take any input.\nUse it only to display messages for the user."
-  );
-  menu();
-}
-
+ function askUserName() {
+  const askUserName = prompt("What is your name:");
+    if (askUserName.length>0) {
+      alert(`Hi, ${askUserName}! Welcome.`);
+      menu();
+    } else {
+      alert("Please, add a valid name.");
+      this.askUserName();
+    }
+  }
+  
+  
+  
 // menu function
 function menu() {
   const choice = parseInt(
-    prompt(
-      "Hello, this is a the prompt function that displays an input and some text.\n" +
-        "We use parseInt() to make sure that we can take an int as a input from the user.\n" +
-        "By default the prompt() function takes all input as strings."
-    )
+
+    prompt(`What do you want to do?
+    1 - Add a task
+    2 - list all tasks 
+    3 - Complete a task
+    4 - List completed tasks
+    5 - Quit
+    `)
   );
+
+  switch (choice) {
+    case 1:
+      const description = prompt("Enter the task description:");
+      taskManager.addTask(description);
+      this.menu();
+      break;
+    case 2:
+       alert(taskManager.listAllTasks());
+      this.menu();
+      break;
+    case 3:
+      const taskId = parseInt(prompt("Entered the ID of the task:"));
+      taskManager.completeTask(taskId);
+      this.menu();
+      break;
+    case 4:
+      taskManager.listCompletedTasks();
+      this.menu();
+      break;
+      case 5:
+        alert("Goodbye!");
+        break;
+    default:
+      alert("Invalid option. Please, Try again.");
+  }
+}   
+
   // add some more3 logic here
   // choose between if/else or switch 
-}
+
 
 // starts with the function asUserName() which triggers the menu() function
 askUserName();
+
+
+
+
+
+
+
