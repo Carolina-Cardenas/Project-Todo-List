5e2313f660255401cd5326aa5b8c3d8caf218d98
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2024-04-03 10:58:39 +0200

    fix the alert messages

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const taskManager = {
   tasks: [ ],
   addTask: function(description){
     const newTask = {
-      id: this.generateUniqueId(),
-      description: description,
-      completed:false
+    id: this.generateUniqueId(),
+    description: description,
+    completed:false
     }
     this.tasks.push(newTask)
   },
@@ -17,56 +17,70 @@ const taskManager = {
     return this.tasks.length + 1;
   }, 
  
- completeTask: function(taskId) {
-  
-    this.tasks.map(function(task){
-    if(task.id == taskId ){
-      task.complete = precioDescuento;
-    }
-}
- )},
+  completeTask: function(taskId) {
+    this.tasks = this.tasks.map(function(task) {
+        if (task.id === taskId) {
+            return { ...task, completed: true };
+        }
+        return task;
+    });
+},
+
 listAllTasks: function(){
-  let taskList=[];
-  this.tasks.forEach(function(task) { 
-  let taskItem = "ID: " + task.id + " - Description: " + task.description + " - Completes: " + task.completed;
-  taskList.push(taskItem);
-  });
-  return taskList;
+  if (this.tasks.length>0) {
+    let taskList=[];
+    this.tasks.forEach(function(task) { 
+    let taskItem = "ID: " + task.id + " - Description: " + task.description + " - Completed: " + task.completed + "\n";
+  
+    taskList.push(taskItem);
+    });
+    alert(taskList);
+  } else {
+    alert("You have no tasks");
+  }
+  menu();
 },
 
 listCompletedTasks: function(){
-completedTaskList = this.tasks.filter(task => task.completed == true)
-this.completedTaskList.forEach(function(task) {
-  var taskItem = document.createElement("li");
-  taskItem.textContent = "ID: " + task.id + " - Descripction: " + task.description + " - Completes: " + task.completed;
-  taskList.appendChild(taskItem);
-  });
-  return taskList;
-},
-};
+if (this.tasks.length>0)  { 
+   const completedTaskList = this.tasks.filter(task => task.completed === true)
+   let taskList=[];
+   completedTaskList.forEach(function(task) {
+   let taskItem = "ID: " + task.id + " - Description: " + task.description + " - Completed: " + task.completed + "\n";
 
+  taskList.push(taskItem);
+  });
+  alert(taskList);
+} else{
+  alert("You have no tasks");
+}
+menu();
+},
+
+}
 
 // function for asking the user to fill in their name
- function askUserName() {
+  function askUserName() {
   const askUserName = prompt("What is your name:");
     if (askUserName.length>0) {
-      alert(`Hi, ${askUserName}! Welcome.`);
+      alert(`Hi,${askUserName}! Welcome.`);
       menu();
     } else {
       alert("Please, add a valid name.");
       this.askUserName();
     }
-  }
+   }
+  
   
   
   
 // menu function
-function menu() {
+  function menu() {
   const choice = parseInt(
 
     prompt(`What do you want to do?
     1 - Add a task
-    2 - list all tasks 
+    2 - List all tasks 
     3 - Complete a task
     4 - List completed tasks
     5 - Quit
@@ -77,26 +91,25 @@ function menu() {
     case 1:
       const description = prompt("Enter the task description:");
       taskManager.addTask(description);
-      this.menu();
+      menu();
       break;
     case 2:
-       alert(taskManager.listAllTasks());
-      this.menu();
+      taskManager.listAllTasks();
       break;
     case 3:
       const taskId = parseInt(prompt("Entered the ID of the task:"));
       taskManager.completeTask(taskId);
-      this.menu();
+      menu();
       break;
     case 4:
-      taskManager.listCompletedTasks();
-      this.menu();
+      (taskManager.listCompletedTasks());
       break;
       case 5:
         alert("Goodbye!");
         break;
     default:
       alert("Invalid option. Please, Try again.");
+      menu();
   }
 }   
 
